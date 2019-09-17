b3e1ffe22b68588a950c9c13e83b7aac37c13ea8
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-09-17 16:06:12 -0700

    add webpack template and sample images

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Median />
+  </div>
+</template>
+
+<script>
+import Median from "./components/Median.vue";
+
+export default {
+  name: "App",
+  components: {
+    Median
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Median.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Median Filter</h1>
+
+    <div class="columns">
+      <div class="column is-6">
+        <img src="/src/assets/33c64780-a9c0-e92a-820c-fae7011c11e2.png" />
+      </div>
+      <div class="column is-6">
+        <img src="/src/assets/33c64780-a9c0-e92a-820c-fae7011c11e2.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import jimp from "jimp";
+
+console.log(jimp);
+
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      x: 1,
+      y: 1
+    };
+  }
+});
+</script>
+
+<style scoped>
+</style>
